80497b329ea26ae97477924c5eec182b8131712b
reemahs0pr0 <shameer_93@hotmail.com>
2020-12-31 12:02:47 +0800

    day30

--- Problem30 - Find Non-Duplicated Integer.py
+++ Problem30 - Find Non-Duplicated Integer.py
@@ -6,13 +6,33 @@
 # Do this in O(N) time and O(1) space.
 
 from Quicksort import quicksort
+import random
 
-arr1 = [6, 1, 3, 3, 3, 6, 6]
-arr2 = [13, 19, 13, 13]
-arr3 = [23, 13, 19, 13, 10, 13, 19, 19, 5, 10, 5, 23, 5, 24, 23, 10]
-quicksort(arr1, 0, len(arr1)-1)
-quicksort(arr2, 0, len(arr2)-1)
-quicksort(arr3, 0, len(arr3)-1)
+def generate(size):
+    low = 1
+    high = 99
+    arr = []
+    used = set()
+    num = random.randint(low, high)
+    for k in range(3):
+        arr.append(num)
+    length = len(arr)
+    used.add(num)
+    for i in range(3, size-1, 3):
+        num = random.randint(low, high)
+        while num in used:
+            num = random.randint(low, high)
+        used.add(num)
+        for j in range(3):
+            index = random.randint(0,length-1)
+            arr.insert(index, num)
+            length += 1
+    num = random.randint(low, high)
+    while num in used:
+        num = random.randint(low, high)
+    index = random.randint(0,length-1)
+    arr.insert(index, num)
+    return arr
 
 def non_duplicate(arr):
     i = 0
@@ -23,7 +43,10 @@ def non_duplicate(arr):
             i += 3
         else:
             return arr[i]
-        
-print(non_duplicate(arr1))
-print(non_duplicate(arr2))
-print(non_duplicate(arr3))
+    
+# arg of generate() is length of arr where length = 3x + 1 and x is the
+# number of repeated integers
+test = generate(7)
+print(test)
+quicksort(test, 0, len(test)-1)
+print(non_duplicate(test))
